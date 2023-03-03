8299f9faecd9dbf799cde98ded98f18cb31ce81e
CasianLW <ciorbcasian@gmail.com>
2023-03-03 16:51:35 +0100

    Controller working, new msg, check msk, delete on show page

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -7,13 +7,13 @@ use App\Mail\MessageSent;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Crypt;
 
 class MessageController extends Controller
 {
     public function create()
 {
     return view('create');
+    // return view('create');
 }
 
 public function store(Request $request)
@@ -23,26 +23,24 @@ public function store(Request $request)
         'message' => 'required|min:15',
     ]);
 
-    $message = Message::create([
+    $message = new Message([
         'message' => $validated['message'],
-        'token' => Crypt::encryptString(Str::random(32)),
+        'token' => Str::random(32),
     ]);
+
+    $message->save();
 
     Mail::to($validated['email'])->send(new MessageSent($message));
 
-    return redirect()->route('message.show', $message->token);
+    return redirect()->route('messages.create')->with('status', 'Your message has been sent!');
 }
 
 public function show(Request $request, $token)
 {
     $message = Message::where('token', $token)->firstOrFail();
 
-    if ($message->read_at) {
-        abort(404);
-    }
-
-    $message->read_at = now();
-    $message->save();
+ 
+    $message->delete();
 
     return view('show', ['message' => $message]);
 }
