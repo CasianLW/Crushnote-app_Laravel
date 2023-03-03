ba56ba1204d0564195ecb2f90605f0002130160b
CasianLW <ciorbcasian@gmail.com>
2023-03-03 16:52:57 +0100

    migration fix

--- database/migrations/2023_03_03_132646_create_messages_table.php
+++ database/migrations/2023_03_03_132646_create_messages_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -12,9 +13,9 @@ return new class extends Migration
     public function up()
 {
     Schema::create('messages', function (Blueprint $table) {
-        $table->id();
+        $table->bigIncrements('id');
         $table->text('message');
-        $table->string('token');
+        $table->string('token')->default(Str::random(32))->unique();
         $table->timestamps();
     });
 }
@@ -25,5 +26,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('messages');
+
     }
 };
